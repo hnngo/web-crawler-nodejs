2f4d1f32bd0b2735767b987886b92d112c690082
Nhan <ngohuynhnhan@gmail.com>
2020-05-16 22:58:32 +0800

    Move idmb project to outer directory

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+const express = require("express");
 const { Command } = require("commander");
+
+// Init needed app
+const app = express();
 const program = new Command();
 
+// Routes for imdb
+const imdbRoutes = require("./routes/imdb");
+imdbRoutes(app);
+
+// Septup command line
 program.version("0.0.1");
 
 program
   .option("-d, --debug", "output extra debugging")
   .option("-s, --small", "small pizza size")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
+  .option("-p, --pizza-type <type>", "flavour of pizza")
+  .parse(process.argv);
 
-program.parse(process.argv);
-
-if (program.debug) console.log(program.opts());
-console.log("pizza details:");
-if (program.small) console.log("- small pizza size");
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
+if (program.debug) {
+  console.log("Debug");
+} else {
+  const PORT = 8000;
+  app.listen(PORT, () => console.log(`Crawling on port ${PORT}....`));
+}
